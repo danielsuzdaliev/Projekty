4254eba6f3d4887cdc9bc47a5c3916fa3d0da8a3
danielsuzdaliev <danielsuzdaliev@gmail.com>
2026-04-01 21:32:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let Tlacitko = document.getElementById("tlacitko")
+let modalID = document.getElementById("modalID")
+
+Tlacitko.addEventListener("click", function() {
+  modalID.style.display = "block"
+})
